26e048df716371fd60fa28c2d51316ef9e09ce79
Rebekah Garris <rebekahlgarris@gmail.com>
2023-10-11 19:07:57 -0500

    starting studio for user input

---
+++ src/main/java/org/launchcode/models/User.java
@@ -0,0 +1,15 @@
+package org.launchcode.models;
+
+public class User {
+    private String username;
+    private String email;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
